4eb9270ca530f6864c030979c46f05675be8737d
Adam Orr <adamorr234@gmail.com>
2016-11-05 16:46:37 +0000

    Initialized project

---
+++ README.md
@@ -0,0 +1,4 @@
+#file-size-getter
+
+Select a file and press submit to view its size in bytes.
+I built this project with node, express.js and multer (https://www.npmjs.com/package/multer)
---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var multer = require('multer');
+var server = express();
+var multer  = require('multer');
+var upload = multer();
+server.listen(process.env.PORT, process.env.IP);
+server.use(express.static('view'));
+
+server.post('/get-file-size', upload.single('image'), function(req, res) {
+  res.send('{"size": ' + req.file.size + '}');
+});
